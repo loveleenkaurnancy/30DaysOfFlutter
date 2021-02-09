7d8dfef6485e99f599de9d0a63720a9fb5720a56
Loveleen Kaur <loveleen.nancy@gmail.com>
2021-02-09 23:30:12 +0530

    Functions, Classes, Routes, Theme & Text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/home_page.dart';
+import 'package:flutter_demo/pages/home_page.dart';
+import 'package:flutter_demo/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +19,17 @@ class MyApp extends StatelessWidget {
     final a = 31;
 
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      darkTheme: ThemeData(primarySwatch: Colors.green),
+      initialRoute: "/home",
+      routes: {
+        "/" : (context) => LoginPage(),
+        "/home" : (context) => HomePage(),
+        "/login" : (context) => LoginPage()
+      },
     );
   }
 }
