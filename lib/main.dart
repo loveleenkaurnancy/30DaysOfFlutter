dac3c83d04f4b71d55bf29c45b81b9d5f3571bfe
Loveleen Kaur <loveleen.nancy@gmail.com>
2021-02-09 22:17:36 +0530

    Dart Data Types & Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_demo/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,14 +8,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    double pi = 3.14;
+    bool isMale = false;
+    num temp = 31.5;
+
+    var day  = "Tuesday";
+    const pie = 31.4;
+    final a = 31;
+
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child : Text("Welcome to 30 days of Flutter"),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
