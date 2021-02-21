402d6b8d25ac20ed0d8effcf52ac7e0330b3a73c
Loveleen Kaur <loveleen.nancy@gmail.com>
2021-02-21 16:17:37 +0530

    ListView Builder, List Generate, Card & Asserts

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,6 +1,22 @@
+class CatalogModel {
+
+  static final items = [
+    Item(
+        id: 1,
+        name: "iPhone 12 Pro",
+        desc: "Mobile Phone",
+        price: 999,
+        color: "#33505a",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRISJ6msIu4AU9_M9ZnJVQVFmfuhfyJjEtbUm3ZK11_8IV9TV25-1uM5wHjiFNwKy99w0mR5Hk&usqp=CAc"
+    )
+  ];
+
+}
+
+
 class Item {
 
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
