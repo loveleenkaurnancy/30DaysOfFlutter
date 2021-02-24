72d61f2e8abd9fd93ae5de2cb9d0f46509fa5093
Loveleen Kaur <loveleen.nancy@gmail.com>
2021-02-24 23:08:11 +0530

    Flutter UI and VelocityX

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
 
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       primarySwatch: Colors.deepPurple,
       appBarTheme: AppBarTheme(
           color: Colors.white,
@@ -17,5 +17,9 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
       brightness: Brightness.dark
   );
+
+  //  Colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 
 }
